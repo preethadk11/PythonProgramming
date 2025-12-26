bfc1b43411ebd75b33f3762b3a466921546805ea
preethadk11 <preethadk11@gmail.com>
2025-12-27 00:46:32 +0530

    variables

---
+++ day-02/sum1.py
@@ -0,0 +1,4 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+avg_test_score=float(input("Enter your average test score: "))
+print(f"hello {name}, you are {age} years old and your test score is {avg_test_score}")
