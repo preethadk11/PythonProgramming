916341705ff82fe86e7f8e63e3b3e30363246063
Preetha <preethadk11@gmail.com>
2025-12-29 19:46:20 +0530

    occurrence of each strings

--- day-06/sum9.py
+++ day-06/sum9.py
@@ -1,10 +1,7 @@
-this_list=[23,"apple",12,"banana","apple"]
-count=0
-new_list=[]
-for i in this_list:
-    if i not in new_list:
-        new_list.append(i)
-        count=0
-    elif i in new_list:
-        count+=1
-        print(i+":",count)
+this_list=["preetha","hello",12,32,12,"priyanga","preetha"]
+counted=[]
+for item in this_list:
+    if item not in counted:
+        count=this_list.count(item)
+        print(item,":",count)
+        counted.append(item)
