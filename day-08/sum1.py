dbb642d6afee1be5ffcaaaf1f1cab9227d5e97ad
Preetha <preethadk11@gmail.com>
2026-01-01 20:52:10 +0530

    pythone file handling

--- day-08/sum1.py
+++ day-08/sum1.py
@@ -18,7 +18,8 @@ if os.path.exists("myfile.txt"):
     print("File exist")
 else:
     print("Not exist")'''
-import os
+'''import os
+print(os.getcwd())
 if os.path.exists("myfile.txt"):
     os.remove("myfile.txt")
 f=open("myfile.txt","w")
@@ -29,4 +30,20 @@ f.write("\nand my name is preetha catherin")
 f.close()
 f=open("myfile.txt","r")
 print(f.read())
-f.close()
+f.close()'''
+import os
+print(os.getcwd())
+if os.path.exists("myfile.txt"):
+    print("befor delete")
+else:
+    print("before delete")
+with open("myfile.txt","w") as f:
+   f.write("Hello everyone")
+with open("myfile.txt","r") as f:
+    print(f.read())
+os.remove("myfile.txt")
+if os.path.exists("myfile.txt"):
+    print("After delete")
+else:
+    print("After delete")
+
