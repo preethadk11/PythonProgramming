45490e116260e70113bb241899eddafda8946a14
Preetha <preethadk11@gmail.com>
2026-01-05 05:23:15 +0530

    sum16.py

---
+++ day-07/sum16.py
@@ -0,0 +1,8 @@
+my_dict={'name':'Alice','age':30,'city':'Newyork','profession':'Doctor'}
+my_dict.pop('profession')
+print(my_dict)
+for x,y in my_dict.items():
+    print(x,":",y)
+    
+if 'age' in my_dict:
+    print(True)
