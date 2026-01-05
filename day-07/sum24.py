e16e51bfb2455625d5bd6d8d15a907f18779cef5
Preetha <preethadk11@gmail.com>
2026-01-05 06:30:30 +0530

    dict comp

---
+++ day-07/sum24.py
@@ -0,0 +1,12 @@
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+
+# Keys to extract
+keys = ["name", "salary"]
+
+res={k: sample_dict[k] for k in keys}
+print(res)
+
