8b72728bbe8c82d4ea201567b5a02efd8823607f
Preetha <preethadk11@gmail.com>
2026-01-05 05:35:52 +0530

    merge dict

---
+++ day-07/sum18.py
@@ -0,0 +1,7 @@
+'''my_dict={'name':'ALICE','age':35,'city':'Newyork'}
+my_dict.clear()
+print(my_dict)'''
+dict1={'Ten':10,'Twenty':20,'Thirty':30}
+dict2={'Thirty':30,'Fourty':40,'Fifty':50}
+dict1.update(dict2)
+print(dict1)
