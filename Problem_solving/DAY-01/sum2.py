f3f2087e7087683ead086799a85c9e1e19f9f486
Preetha <preethadk11@gmail.com>
2026-02-01 19:18:13 +0530

    sizeof data types

---
+++ Problem_solving/DAY-01/sum2.py
@@ -0,0 +1,7 @@
+import sys
+print("Size of int: ",sys.getsizeof(int))
+print("Size of float: ",sys.getsizeof(float))
+print("Size of float: ",sys.getsizeof(str))
+print("Size of float: ",sys.getsizeof(bytes))
+print("Size of int: ",sys.getsizeof(4))
+print("Size of float: ",sys.getsizeof(78.09))
