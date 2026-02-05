e22758238d7ac5b2f14aade72934ad6fa4a3a6ff
Preetha <preethadk11@gmail.com>
2026-02-06 00:34:26 +0530

    number pattern

--- Problem_solving/DAY-09/sum1.py
+++ Problem_solving/DAY-09/sum1.py
@@ -1,4 +1,9 @@
-for i in range(1,5):
-    for j in range(i):
-        print(chr(65+j),end="")
+n=4
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    for s in range(2*(n-i)):
+        print(" ",end="")
+    for j in range(i,0,-1):
+        print(j,end="")
     print()
