689a38b0580a20fa714e3b873c80b99c636a042f
Preetha <preethadk11@gmail.com>
2026-02-08 01:25:37 +0530

    star pattern

---
+++ Problem_solving/DAY-17/sum1.py
@@ -0,0 +1,8 @@
+for i in range(1,5+1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+for i in range(5,1,-1):
+    for j in range(1,i):
+        print("*",end=" ")
+    print()
