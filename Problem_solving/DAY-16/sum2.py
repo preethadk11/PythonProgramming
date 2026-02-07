690e1f4d4b4988e2efd2d73bc1cb8da070e6ea85
Preetha <preethadk11@gmail.com>
2026-02-07 18:59:48 +0530

    all_digit and alphabet using for loop

---
+++ Problem_solving/DAY-16/sum2.py
@@ -0,0 +1,10 @@
+print("Digits: ",end=" ")
+for i in range(0,10):
+    print(i,end="")
+print()
+print("Alphabets: ",end="")
+for j in range(1,27):
+    print(chr(64+j),end="")
+print(" ",end="")
+for j in range(1,27):
+    print(chr(96+j),end="")
