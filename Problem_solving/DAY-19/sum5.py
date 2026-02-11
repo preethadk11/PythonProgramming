f8968d3e6068f601ae6e6ba61c5a4ca812dc592d
Preetha <preethadk11@gmail.com>
2026-02-12 00:13:21 +0530

    concentric alphabet

--- Problem_solving/DAY-19/sum5.py
+++ Problem_solving/DAY-19/sum5.py
@@ -3,5 +3,5 @@ size=2*n-1
 for i in range(size):
     for j in range(size):
         val =n - min(i,j,size-i-1,size-j-1)
-        print(val,end=" ")
+        print(chr(ord('A')+val-1),end=" ")
     print()
