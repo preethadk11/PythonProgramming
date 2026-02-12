a0a625bdb7872535c3360cf91ff3244d2cea836f
Preetha <preethadk11@gmail.com>
2026-02-12 19:08:22 +0530

    concentric patter

--- Problem_solving/DAY-19/sum5.py
+++ Problem_solving/DAY-19/sum5.py
@@ -3,5 +3,6 @@ size=2*n-1
 for i in range(size):
     for j in range(size):
         val =n - min(i,j,size-i-1,size-j-1)
-        print(chr(ord('A')+val-1),end=" ")
+        #print(chr(ord('A')+val-1),end=" ")
+        print(chr(65+val-1),end="  ")
     print()
