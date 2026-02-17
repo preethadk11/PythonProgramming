e904e97611a21b277748236608921c94eecceea9
Preetha <preethadk11@gmail.com>
2026-02-17 21:56:48 +0530

    matrix addition

---
+++ Problem_solving/DAY-28/sum3.py
@@ -0,0 +1,11 @@
+a=[[1,2],[3,4]]
+b=[[5,6],[7,8]]
+rows=len(a)
+cols=len(a[0])
+result=[]
+for i in range(rows):
+    row=[]
+    for j in range(cols):
+        row.append(a[i][j]+b[i][j])
+    result.append(row)
+print(result)
